363935cc811d8256173ddf52477ea37867d028f2
sumit keshari <kesarwanisumit954@gmail.com>
2025-06-22 16:45:43 +0530

    update api because it's not working

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,4 +4,4 @@ export const LOGO_URL =
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-  export const RESTOMENU= 'https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=';
+  export const RESTOMENU= 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=';
